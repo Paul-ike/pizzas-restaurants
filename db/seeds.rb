f8b6565d94a7f1e33be114d2af741f660219ab60
Paul <paul.ngugi@student.moringaschool.com>
2023-03-17 22:15:12 +0300

    associated the models, created migrations for each table, created seed database and created routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+pizzas = Pizza.create([{ name: "Cheese", ingredients: "Dough, Tomato Sauce, Cheese" }, { name: "Pepperoni", ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni" }])
+
+restaurants = Restaurant.create([{ name: "Sottocasa NYC", address: "298 Atlantic Ave, Brooklyn, NY 11201" }, { name: "PizzArte", address: "69 W 55th St, New York, NY 10019" }])
+
+restaurant_pizzas = RestaurantPizza.create([{ price: 5, pizza_id: 1, restaurant_id: 2 }, { price: 12, pizza_id: 1, restaurant_id: 1 }, { price: 24, pizza_id: 2, restaurant_id: 2 }, { price: 19, pizza_id: 2, restaurant_id: 2 } ])
